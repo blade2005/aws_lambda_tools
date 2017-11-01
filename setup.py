60157af607f6917f6045cd834fb806b59d4ba000
Craig Davis <cdavis@alertlogic.com>
2017-11-01 10:38:58 -0500

    bugfix from copied setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-name = 'agilepoint'
+name = 'aws_lambda'
 version_file = open(path.join(name, 'VERSION'))
 version = version_file.read().strip()
 setup(
